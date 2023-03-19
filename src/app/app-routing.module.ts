d7fde0b9961a81801436f18be82d3003b4109022
PavelSaganov <saganovpavel@mail.ru>
2023-03-19 20:00:44 +0300

    Added guard for order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartListComponent } from './cart/components/cart-list/cart-list.component';
+import { ProcessOrderComponent } from './orders/components/process-order/process-order.component';
+import { CartGuard } from './orders/guards/cart.guard';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
 import { ProductViewComponent } from './products/components/product-view/product-view.component';
 
@@ -19,11 +21,18 @@ const routes: Routes = [
     path: 'cart',
     component: CartListComponent,
     title: 'Your cart'
+  },
+  {
+    path: 'order',
+    component: ProcessOrderComponent,
+    title: 'Processing order',
+    canActivate: [CartGuard]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CartGuard]
 })
 export class AppRoutingModule { }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProcessOrderComponent } from './components/process-order/process-order.component';
+import { CartGuard } from './guards/cart.guard';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProcessOrderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    CartGuard
   ]
 })
 export class OrdersModule { }
